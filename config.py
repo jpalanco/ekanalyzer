bea0f0dc22e6deefab56315955b91ce4f4b8be45
Jose Ramon Palanco <jose.palanco@drainware.com>
2014-09-22 12:43:26 +0200

    Recovered conf file

---
+++ config.py
@@ -0,0 +1,19 @@
+
+
+UPLOAD_FOLDER = "uploads/"
+MAX_CONTENT_LENGTH= 16 * 1024 * 1024
+
+
+CELERY_RESULT_BACKEND = "redis"
+CELERY_REDIS_HOST = "localhost"
+CELERY_REDIS_PORT = 6379
+CELERY_REDIS_DB = 0
+
+BROKER_URL = 'redis://localhost:6379/0'
+MONGODB_SERVER = 'localhost'
+MONGODB_PORT = 27017
+
+
+USER_AGENTS = open("user_agents.txt").read().splitlines()
+
+VIRUSTOTAL_API_KEY = "PUT HERE YOUR API KEY"
